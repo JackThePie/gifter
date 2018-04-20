6acb8da33ff635f40079a987b5a6ccd3c21fc3e3
Jacek Bera <jbera@egnyte.com>
2018-04-20 15:56:49 +0200

    Trying new fields:

--- main_app/models.py
+++ main_app/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Gift(models.Model):
     name = models.CharField(max_length=100)
     link = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, default="jacool92@gmail.com")
+    occupied = models.BooleanField(default=0)
 
     def __str__(self):
         return self.name
