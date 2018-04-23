2d903d43ba1284d4b8609a42603a3dd931688c8c
JackThePie <jacool92@gmail.com>
2018-04-24 00:07:51 +0200

    Like button and database update

--- main_app/models.py
+++ main_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Gift(models.Model):
     name = models.CharField(max_length=100)
-    link = models.CharField(max_length=100)
+    link = models.URLField(max_length=100)
     email = models.EmailField(max_length=100, default="jacool92@gmail.com")
     occupied = models.BooleanField(default=0)
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from .models import Gift
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     gifts = Gift.objects.all()
     return render(request, 'index.html', {'gifts': gifts})
 
+def occupy(request):
+    gift_id = request.GET.get('gift_id', None)
+
+    occupied = 0
+    if (gift_id):
+        gift = Gift.objects.get(id=int(gift_id))
+        if gift is not None:
+            occupied = gift.occupied = 1
+            gift.occupied = occupied
+            gift.save()
+    return HttpResponse(occupied)
 
 # class Gift:
 #     def __init__(self, name, link):
