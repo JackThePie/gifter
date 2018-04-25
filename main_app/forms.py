dae92007d89d04650ad4e698fac528b0a1df7734
JackThePie <jacool92@gmail.com>
2018-04-26 00:31:54 +0200

    Working form

--- main_app/forms.py
+++ main_app/forms.py
@@ -2,4 +2,3 @@ from django import forms
 
 class ContactForm(forms.Form):
     contact_email = forms.EmailField(required=True)
-    name = forms.CharField(label="LOLOLO", max_length=100)
--- main_app/views.py
+++ main_app/views.py
@@ -1,42 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Gift
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.core.mail import EmailMessage
 from .forms import ContactForm
+from django.template.loader import get_template
 
 # Create your views here.
 def index(request):
     gifts = Gift.objects.all()
-    return render(request, 'index.html', {'gifts': gifts})
+    form_class = ContactForm
 
-# def occupy(request):
-#     gift_id = request.GET.get('gift_id', None)
-#
-#     occupied = 0
-#     if (gift_id):
-#         gift = Gift.objects.get(id=int(gift_id))
-#         if gift is not None and gift.occupied != 0:
-#             occupied = gift.occupied = 1
-#             gift.occupied = occupied
-#             gift.save()
-#             # send_mail(
-#             #     'Test',
-#             #     'testtest',
-#             #     'jacool92@gmail.com',
-#             #     ['jacool92@gmail.com'],
-#             #     fail_silently=False,
-#             # )
-#     form = ContactForm
-#     if form:
-#         email = EmailMessage('test', 'testteset', to=['jacool92@gmail.com'])
-#         email.send()
-#     return HttpResponse(occupied)
+    if request.method == 'POST':
+        print("JUUUPI")
+        form = form_class(data=request.POST)
+        if form.is_valid():
+            contact_email = request.POST.get('contact_email', '')
+            email = EmailMessage('test', 'testteset', to=['jacool92@gmail.com'])
+            email.send()
+
+    return render(request, 'index.html', {'gifts': gifts,
+                                          'form': form_class})
 
 def occupy(request):
-    form = ContactForm()
+    gift_id = request.GET.get('gift_id', None)
 
-    return render(request, 'index.html', {'form': form,
+    occupied = 0
+    if (gift_id):
+        gift = Gift.objects.get(id=int(gift_id))
+        if gift is not None and gift.occupied != 0:
+            occupied = gift.occupied = 1
+            gift.occupied = occupied
+            gift.save()
+            # send_mail(
+            #     'Test',
+            #     'testtest',
+            #     'jacool92@gmail.com',
+            #     ['jacool92@gmail.com'],
+            #     fail_silently=False,
+            # )
+        if form:
+            email = EmailMessage('test', 'testteset', to=['jacool92@gmail.com'])
+            email.send()
+    return HttpResponse(occupied)
+
+def contact(request):
+    form_class = ContactForm()
+
+    return render(request, 'index.html', {'form': form_class,
                                             })
 
 
