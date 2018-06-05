cfd7e09b097ab604a3167d0896a703190f60258b
Jacek Bera <jbera@egnyte.com>
2018-06-05 16:53:59 +0200

    Trying to catch form

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -1,22 +1,24 @@
-//$('button').on('click', function(event){
-//    event.preventDefault();
-//    var element = $(this);
-//    $.ajax({
-//        url : '/occupy/',
-//        type : 'GET',
-//        data : { gift_id : element.attr("data-id")},
-//        success : function(response){
-//            element.html(' ' + response);
-//        }
-//    });
-//});
-
-$('#form').submit(function(e){
+$('button').on('click', function(event){
     event.preventDefault();
     var element = $(this);
     $.ajax({
         url : '/occupy/',
         type : 'GET',
+        data : { gift_id : element.attr("data-id")},
+        success : function(response){
+            element.html(' ' + response);
+        }
+    });
+});
+
+$('#form').submit(function(event){
+    event.preventDefault();
+    var formData = $(this).serialize();
+    var element = $(this);
+    $.ajax({
+        url : '/occupy/',
+        type : 'GET',
+//        data : formData,
         data : { gift_id : element.attr("data-id")},
         success : function(response){
             element.html(' ' + response);
