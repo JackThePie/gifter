b0d9311793894093379079198ecd4f8d148cb306
JackThePie <jacool92@gmail.com>
2018-06-06 23:58:17 +0200

    Turned off debug mode

--- gifter/settings.py
+++ gifter/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h62te-%4=g$%-_zm_=#hx^*ybmj*vlioas-i)-xzjqnsr4)hpa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['klaraijacek.pythonanywhere.com',
                  '127.0.0.1']
