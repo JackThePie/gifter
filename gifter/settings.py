045f39a0792ff94e52faa61af93c7610dd2631b7
JackThePie <jacool92@gmail.com>
2018-06-07 00:04:40 +0200

    Fixed static files path

--- gifter/settings.py
+++ gifter/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h62te-%4=g$%-_zm_=#hx^*ybmj*vlioas-i)-xzjqnsr4)hpa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['klaraijacek.pythonanywhere.com',
                  '127.0.0.1']
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '../../static/'
 
 
 # EMAIL_BECKEND = 'django.core.mail.backends.console.EmailBackend'
