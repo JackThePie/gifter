6b6e5dc7466345fc07ffbfdac4fdd260b60daff6
Jacek Bera <jbera@egnyte.com>
2018-06-21 14:56:48 +0200

    Link length fixed

--- main_app/models.py
+++ main_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Gift(models.Model):
     name = models.CharField(max_length=100)
-    link = models.URLField(max_length=100)
+    link = models.URLField(max_length=300)
     email = models.EmailField(blank=True, default='')
     occupied = models.BooleanField(default=0)
 
