805cbd6f19a129f25186b82a3bb1165001de8a25
Jacek Bera <jbera@egnyte.com>
2018-06-21 15:08:16 +0200

    Added ROOT_PATH

--- gifter/settings.py
+++ gifter/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 # EMAIL_BECKEND = 'django.core.mail.backends.console.EmailBackend'
